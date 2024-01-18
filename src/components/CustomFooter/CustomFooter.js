c163645aca56b4613ce9786c04dcbaad89c857c7
Alan Sapet <alansapetind@gmail.com>
2024-01-18 14:04:04 +0100

    Fixed issues with customFooter

--- src/components/CustomFooter/CustomFooter.js
+++ src/components/CustomFooter/CustomFooter.js
@@ -1,24 +1,26 @@
 import React from "react";
-import {View, Text, TextInput, StyleSheet, Image} from "react-native";
+import {View, Text, TextInput, StyleSheet, Image, Dimensions} from "react-native";
 import { useNavigation } from '@react-navigation/native';
+import Images from "../../../assets/index.js";
 
 const CustomFooter = ({currentOption = "home"}) =>{
-
-    let home = "home_locked";
-    let search = "search_locked";
-    let chat = "chat_locked";
+    const appWidth = Dimensions.get('window').width;
+    const appHeight = Dimensions.get('window').height;
+    let home = Images.home_locked;
+    let search = Images.search_locked;
+    let chat = Images.chat_locked;
 
     switch (currentOption){
       case "home" :
-        home = "home";
+        home = Images.home;
         break;
 
       case "search" :
-        search = "search";
+        search = Images.search;
         break;
       
       case "chat" :
-        chat = "chat";
+        chat = Images.chat;
         break;
     }
 
@@ -39,15 +41,27 @@ const CustomFooter = ({currentOption = "home"}) =>{
     }
     
   return(
-      <View style={styles.footer}>
+      <View style={{display: "flex", 
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "#292827",
+        position:"absolute",
+        paddingLeft:40, 
+        paddingRight:40, 
+        paddingTop:15, 
+        paddingBottom:15,
+        marginBottom:0,
+        top: appHeight-25,
+        width: appWidth}}>
         <View>
-            <Image style={{ width: 25, height: 25}} source={require("../../../assets/home_locked.png")} onClick={() => goHome()}></Image>
+            <Image style={{ width: 25, height: 25}} source={home}></Image>
+            {/* <Image style={{ width: 25, height: 25}} source={require("../../../assets/home.png")} onClick={() => goHome()}></Image> */}
         </View>
         <View>
-            <Image style={{ width: 25, height: 25}} source={require("../../../assets/home_locked.png")} onClick={() => goSearch()}></Image>
+            <Image style={{ width: 25, height: 25}} source={search} onClick={() => goSearch()}></Image>
         </View>
         <View>
-            <Image style={{ width: 25, height: 25}} source={require("../../../assets/home_locked.png")} onClick={() => goChat()}></Image>
+            <Image style={{ width: 25, height: 25}} source={chat} onClick={() => goChat()}></Image>
         </View>
       </View>)
   }
@@ -55,13 +69,6 @@ const CustomFooter = ({currentOption = "home"}) =>{
 
 const styles = StyleSheet.create({
     footer: {
-        display: "flex", 
-        flexDirection: "row",
-        justifyContent: "space-between",
-        backgroundColor: "#292827", 
-        paddingLeft:"40px", 
-        paddingRight:"40px", 
-        paddingTop:"12px", 
-        paddingBottom:"12px"
+
       }})
 export default CustomFooter
