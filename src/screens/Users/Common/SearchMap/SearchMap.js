f2e22d46bf526997e1930ea7ac3e588d163795e4
Yohann <73314529+yohann69@users.noreply.github.com>
2024-01-28 14:12:08 +0100

    💪 Smoother animations

--- src/screens/Users/Common/SearchMap/SearchMap.js
+++ src/screens/Users/Common/SearchMap/SearchMap.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import { StyleSheet, View } from "react-native"
 import { useTranslation } from "react-i18next"
-import MapView, { Circle, Marker, PROVIDER_GOOGLE } from "react-native-maps"
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps"
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete" // Doc: https://github.com/FaridSafi/react-native-google-places-autocomplete#readme
 // navigator.geolocation = require('@react-native-community/geolocation');
 navigator.geolocation = require("react-native-geolocation-service")
@@ -9,10 +9,10 @@ navigator.geolocation = require("react-native-geolocation-service")
 const SearchMap = () => {
 	const { t } = useTranslation()
 	const [selectedPlace, setSelectedPlace] = useState(null)
+	const mapRef = useRef(null)
 
 	const handlePlaceSelected = (data) => {
 		let placeId = data.place_id
-		console.log("placeId: ", placeId)
 
 		try {
 			fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBznSC8S1mPU-GPjsxuagQqnNK3a8xVOl4&place_id=${placeId}`)
@@ -21,60 +21,52 @@ const SearchMap = () => {
 				let latitude = json.results[0].geometry.location.lat
 				let longitude = json.results[0].geometry.location.lng
 				setSelectedPlace({
-					latitude: latitude,
-					longitude: longitude,
+					latitude,
+					longitude,
 				})
+
+				mapRef.current.animateToRegion({
+					latitude, longitude, latitudeDelta: 0.0022, longitudeDelta: 0.0021,
+				}, 2000)
 			})
 			.catch((error) => {
 				console.error(error)
 			})
 		} catch (e) {
-			console.log("Error: ", e)
+			console.error("Error: ", e)
 		}
 	}
+
 
 	return (<View style={styles.root}>
 		<View style={styles.mapContainer}>
 			<MapView
+			  ref={mapRef}
 			  provider={PROVIDER_GOOGLE}
 			  style={styles.map}
-			  region={selectedPlace ? {
-				  latitude: selectedPlace.latitude,
-				  longitude: selectedPlace.longitude,
-				  latitudeDelta: 0.0922,
-				  longitudeDelta: 0.0421,
-			  } : {
+			  region={{
 				  latitude: 44.933393,
 				  longitude: 4.892703,
 				  latitudeDelta: 0.0922,
 				  longitudeDelta: 0.0421,
 			  }}
 			  showUserLocation={true}
+			  showsMyLocationButton={true}
 			>
-				{selectedPlace && (
-				  <Marker
+				{selectedPlace && (<Marker
 					coordinate={{
 						latitude: selectedPlace.latitude,
 						longitude: selectedPlace.longitude,
 					}}
-				  />
-				)}
-				<Circle center={{
-					latitude: 44.933393, longitude: 4.892703,
-				}}
-				        radius={500}
-				        fillColor="rgba(255, 0, 0, 0.1)"
-				        strokeColor="rgba(255, 0, 0, 0.1)"
-				        strokeWidth={1}
-				/>
-				<Circle center={{
-					latitude: 44.933993, longitude: 4.886703,
-				}}
-				        radius={500}
-				        fillColor="rgba(0, 155, 100, 0.2)"
-				        strokeColor="rgba(0, 155, 100, 0.2)"
-				        strokeWidth={1}
-				/>
+				  />)}
+				{/*<Circle center={{*/}
+				{/*	latitude: 44.933393, longitude: 4.892703,*/}
+				{/*}}*/}
+				{/*        radius={500}*/}
+				{/*        fillColor="rgba(255, 0, 0, 0.1)"*/}
+				{/*        strokeColor="rgba(255, 0, 0, 0.1)"*/}
+				{/*        strokeWidth={1}*/}
+				{/*/>*/}
 			</MapView>
 
 			<GooglePlacesAutocomplete
@@ -84,7 +76,6 @@ const SearchMap = () => {
 				  key: "AIzaSyBznSC8S1mPU-GPjsxuagQqnNK3a8xVOl4",
 				  language: "fr",
 				  components: "country:fr", // Limit the search to France
-
 			  }}
 			  nearbyPlacesAPI="GooglePlacesSearch"
 			  debounce={400}
@@ -107,7 +98,6 @@ const SearchMap = () => {
 			  }}
 			  currentLocation={true}
 			  currentLocationLabel="Current location"
-
 			/>
 		</View>
 	</View>)
