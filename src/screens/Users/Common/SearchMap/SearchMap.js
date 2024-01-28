345966f0a44aba9118aab179db01d18e22d6408b
Yohann <73314529+yohann69@users.noreply.github.com>
2024-01-28 18:26:08 +0100

    🕒 Adding dateTime picker

--- src/screens/Users/Common/SearchMap/SearchMap.js
+++ src/screens/Users/Common/SearchMap/SearchMap.js
@@ -1,15 +1,43 @@
 import React, { useRef, useState } from "react"
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 import { useTranslation } from "react-i18next"
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps"
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete" // Doc: https://github.com/FaridSafi/react-native-google-places-autocomplete#readme
-// navigator.geolocation = require('@react-native-community/geolocation');
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
+import DateTimePickerModal from "react-native-modal-datetime-picker"
+import CustomButton from "../../../../components/CustomButton" // https://www.npmjs.com/package/react-native-modal-datetime-picker
+
 navigator.geolocation = require("react-native-geolocation-service")
 
 const SearchMap = () => {
 	const { t } = useTranslation()
 	const [selectedPlace, setSelectedPlace] = useState(null)
 	const mapRef = useRef(null)
+
+	const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
+	const [date, setDate] = useState(new Date())
+
+	const options = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+	}
+
+	const showDatePicker = () => {
+		setDatePickerVisibility(true)
+	}
+
+	const hideDatePicker = () => {
+		setDatePickerVisibility(false)
+	}
+
+	const handleConfirm = (date) => {
+		setDate(date)
+		hideDatePicker()
+	}
+
 
 	const handlePlaceSelected = (data) => {
 		let placeId = data.place_id
@@ -37,7 +65,6 @@ const SearchMap = () => {
 		}
 	}
 
-
 	return (<View style={styles.root}>
 		<View style={styles.mapContainer}>
 			<MapView
@@ -54,11 +81,11 @@ const SearchMap = () => {
 			  showsMyLocationButton={true}
 			>
 				{selectedPlace && (<Marker
-					coordinate={{
-						latitude: selectedPlace.latitude,
-						longitude: selectedPlace.longitude,
-					}}
-				  />)}
+				  coordinate={{
+					  latitude: selectedPlace.latitude,
+					  longitude: selectedPlace.longitude,
+				  }}
+				/>)}
 				{/*<Circle center={{*/}
 				{/*	latitude: 44.933393, longitude: 4.892703,*/}
 				{/*}}*/}
@@ -79,26 +106,34 @@ const SearchMap = () => {
 			  }}
 			  nearbyPlacesAPI="GooglePlacesSearch"
 			  debounce={400}
-			  styles={{
-				  container: {
-					  position: "absolute", top: 100, left: 10, right: 10, borderRadius: 20,
-				  }, textInputContainer: {
-					  width: "100%", borderRadius: 20,
-				  }, textInput: {
-					  borderRadius: 100,
-				  }, poweredContainer: {
-					  display: "none",
-				  }, listView: {
-					  borderRadius: 20, backgroundColor: "#ffc9a8",
-				  }, row: {
-					  backgroundColor: "#ffc9a8", padding: 13, height: 50, flexDirection: "row", borderRadius: 20,
-				  }, separator: {
-					  height: 0.5, backgroundColor: "#3400fd",
-				  },
-			  }}
+			  styles={styles.searchBar}
 			  currentLocation={true}
 			  currentLocationLabel="Current location"
 			/>
+		</View>
+		<View style={styles.detailsBox}>
+			<Text>Details</Text>
+
+			<View>
+				<CustomButton
+				  text={Intl.DateTimeFormat("fr-FR", options).format(date).charAt(0).toUpperCase() + Intl.DateTimeFormat("fr-FR", options).format(date).slice(1)}
+				  onPress={showDatePicker}
+				bgColor={"#ffdba1"}
+				fgColor={"black"}/>
+				<DateTimePickerModal
+				  isVisible={isDatePickerVisible}
+				  mode="datetime"
+				  onConfirm={handleConfirm}
+				  onCancel={hideDatePicker}
+				  date={date}
+				/>
+			</View>
+
+			<CustomButton
+			  text={t("common.next")}
+			  bgColor={"#ff8000"}
+			/>
+
 		</View>
 	</View>)
 }
@@ -110,6 +145,42 @@ const styles = StyleSheet.create({
 		height: "100%", width: "100%", justifyContent: "flex-end", alignItems: "center",
 	}, map: {
 		...StyleSheet.absoluteFillObject,
+	},
+	searchBar: {
+		container: {
+			position: "absolute",
+			top: 50,
+			left: 10,
+			right: 10,
+			borderRadius: 20,
+			shadowColor: "#000",
+			elevation: 10,
+		}, textInputContainer: {
+			width: "100%", borderRadius: 20,
+		}, textInput: {
+			borderRadius: 100,
+		}, poweredContainer: {
+			display: "none",
+		}, listView: {
+			borderRadius: 20, backgroundColor: "#ffc9a8",
+		}, row: {
+			backgroundColor: "#ffc9a8", padding: 13, height: 50, flexDirection: "row", borderRadius: 20,
+		}, separator: {
+			height: 0.5, backgroundColor: "#3400fd",
+		},
+	},
+	detailsBox: {
+		position: "absolute",
+		bottom: 0,
+		left: 0,
+		right: 0,
+		height: "60%",
+		backgroundColor: "white",
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		padding: 25,
+		display: "flex",
+		justifyContent: "space-between",
 	},
 })
 
