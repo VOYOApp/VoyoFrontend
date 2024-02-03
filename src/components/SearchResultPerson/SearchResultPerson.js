341c24c2700ead237f72dbf97514fd9e4f6c2ba1
Yohann <73314529+yohann69@users.noreply.github.com>
2024-02-03 13:01:22 +0100

    Tests For ethan

--- src/components/SearchResultPerson/SearchResultPerson.js
+++ src/components/SearchResultPerson/SearchResultPerson.js
@@ -3,36 +3,45 @@ import { Image, StyleSheet, Text, View } from "react-native"
 import CustomButton from "../CustomButton"
 import { useTranslation } from "react-i18next"
 
-const SearchResultPerson = () => {
+const SearchResultPerson = (data) => {
 	const { t } = useTranslation()
+
+	data = data.data
+
+	// Screen width
+	const width = window.innerWidth
+	styles.rowWithImage.maxWidth = width - (styles.profilePicture.width + styles.btn.marginRight + 80 + 20 + 10 + 10)
 
 	return (
 	  <View style={styles.card}>
-		  <View style={styles.profilePicture}>
-			  <Image source={require("../../../assets/icons/003-add.png")} style={styles.logo} />
-		  </View>
+
+		  <Image
+			src={data.profilePicture}
+			style={[styles.profilePicture]}
+			resizeMode="contain" />
+
 
 		  <View>
 			  <View style={styles.rowWithImage}>
 				  <Image source={require("../../../assets/icons/010-user.png")} style={styles.logo} />
-				  <Text>Bernard</Text>
+				  <Text>{data.name}</Text>
 			  </View>
 			  <View style={styles.rowWithImage}>
-				  <Image source={require("../../../assets/icons/013-clock.png")} style={styles.logo} />
-				  <Text>14 janvier 2032</Text>
+				  <Image source={require("../../../assets/icons/star.png")} style={styles.logoBigger} />
+				  <Text>{data.note}/5</Text>
 			  </View>
 			  <View style={styles.rowWithImage}>
-				  <Image source={require("../../../assets/icons/009-location.png")} style={styles.logoBigger} />
-				  <Text>5 rue </Text>
+				  <Image source={require("../../../assets/icons/long-distance.png")} style={styles.logoBigger} />
+				  <Text>{data.distance}</Text>
 			  </View>
 			  <View style={styles.rowWithImage}>
-				  <Image source={require("../../../assets/icons/014-check.png")} style={styles.logo} />
-				  <Text>Rendez-</Text>
+				  <Image source={require("../../../assets/icons/008-dollar.png")} style={styles.logo} />
+				  <Text>{data.price}€/h</Text>
 			  </View>
 		  </View>
 
 		  <View style={styles.btn}>
-			  <CustomButton text={t("common.details")} widthBtn={80} heightBtn={40} />
+			  <CustomButton text={"RDV"} widthBtn={80} heightBtn={40} />
 		  </View>
 	  </View>
 
@@ -68,20 +77,19 @@ const styles = StyleSheet.create({
 		display: "flex",
 		flexDirection: "row",
 		alignItems: "center",
-		marginBottom: 5,
-		marginTop: 5,
-		width: "100%",
-		maxWidth: 200,
+		marginBottom: 3,
+		marginTop: 3,
+		marginLeft: 7,
+		maxWidth: 150,
 	},
 	logo: {
-		maxWidth: 23,
-		maxHeight: 23,
-		// marginLeft: 10,
+		maxWidth: 20,
+		maxHeight: 20,
 		marginRight: 12,
 	},
 	logoBigger: {
-		maxWidth: 27,
-		maxHeight: 27,
+		maxWidth: 25,
+		maxHeight: 25,
 		marginLeft: -2,
 		marginRight: 10,
 	},
--- src/screens/Users/Common/SearchMap/SearchMap.js
+++ src/screens/Users/Common/SearchMap/SearchMap.js
@@ -1,13 +1,14 @@
 import React, { useCallback, useMemo, useRef, useState } from "react"
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native"
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from "react-native"
 import { useTranslation } from "react-i18next"
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps"
+import MapView, { Circle, Marker, PROVIDER_GOOGLE } from "react-native-maps"
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
 import DateTimePickerModal from "react-native-modal-datetime-picker"
 import CustomButton from "../../../../components/CustomButton" // https://www.npmjs.com/package/react-native-modal-datetime-picker
 import { RadioButton } from "react-native-paper"
 import { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet"
 import SearchResultPerson from "../../../../components/SearchResultPerson"
+import { Picker } from "@react-native-picker/picker"
 
 navigator.geolocation = require("react-native-geolocation-service")
 
@@ -16,7 +17,7 @@ const SearchMap = () => {
 	const bottomSheetModalRef = useRef(null)
 
 	// variables
-	const snapPoints = useMemo(() => ["20%", "60%"], [])
+	const snapPoints = useMemo(() => [130, 550], [])
 
 	// callbacks
 	const handlePresentModalPress = useCallback(() => {
@@ -36,6 +37,45 @@ const SearchMap = () => {
 	const [showResults, setShowResults] = useState(false)
 	const [showDuration, setShowDuration] = useState(false)
 	const [showDate, setShowDate] = useState(false)
+
+
+	const [searchResults, setSearchResults] = useState([{
+		profilePicture: "https://randomuser.me/api/portraits/women/69.jpg",
+		name: "Marie K.",
+		note: 3,
+		distance: 5989,
+		price: 8.8,
+		map: {
+			coordinates: { "latitude": 44.91689119999999, "longitude": 4.9328504 }, radius: 1000,
+		},
+	}, {
+		profilePicture: "https://randomuser.me/api/portraits/men/95.jpg",
+		name: "Jean T.",
+		note: 5,
+		distance: 49,
+		price: 12,
+		map: {
+			coordinates: { "latitude": 44.925527, "longitude": 4.92091 }, radius: 1500,
+		},
+	}, {
+		profilePicture: "https://randomuser.me/api/portraits/women/59.jpg",
+		name: "Bertha F.",
+		note: 4,
+		distance: 489,
+		price: 10,
+		map: {
+			coordinates: { "latitude": 44.933393, "longitude": 4.89236 }, radius: 2000,
+		},
+	}, {
+		profilePicture: "https://randomuser.me/api/portraits/women/59.jpg",
+		name: "Bertha F.",
+		note: 4,
+		distance: 1,
+		price: 10,
+		map: {
+			coordinates: { "latitude": 44.920177, "longitude": 4.899143 }, radius: 1700,
+		},
+	}])
 
 	const options = {
 		weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric",
@@ -74,8 +114,6 @@ const SearchMap = () => {
 					latitude, longitude,
 				})
 
-				// latitude = json.results[0].geometry.location.lat - 0.001
-
 				// Get screen dimensions
 				const { height } = Dimensions.get("window")
 
@@ -100,14 +138,31 @@ const SearchMap = () => {
 
 	const [selectedValue, setSelectedValue] = useState("")
 
-	const radioItems = [
-		{ label: t("prospect.visits_duration.studio"), value: "option1" },
-		{ label: t("prospect.visits_duration.t2_to_t4"), value: "option2" },
-		{ label: t("prospect.visits_duration.t5_and_more"), value: "option3" },
-		{ label: t("prospect.visits_duration.house"), value: "option4" },
-		{ label: t("prospect.visits_duration.villa"), value: "option5" },
-		// { label: t("prospect.visits_duration.custom_duration"), value: "option6" },
+	const radioItems = [{
+		label: t("prospect.visits_duration.studio"), value: "option1",
+	}, {
+		label: t("prospect.visits_duration.t2_to_t4"), value: "option2",
+	}, {
+		label: t("prospect.visits_duration.t5_and_more"), value: "option3",
+	}, { label: t("prospect.visits_duration.house"), value: "option4" }, {
+		label: t("prospect.visits_duration.villa"), value: "option5",
+	}, // { label: t("prospect.visits_duration.custom_duration"), value: "option6" },
 	]
+
+
+	const sortResults = (itemValue) => {
+		let sortedResults = []
+
+		if (itemValue === "stars") {
+			sortedResults = [...searchResults].sort((a, b) => b.note - a.note)
+		} else if (itemValue === "price") {
+			sortedResults = [...searchResults].sort((a, b) => a.price - b.price)
+		} else if (itemValue === "distance") {
+			sortedResults = [...searchResults].sort((a, b) => a.distance - b.distance)
+		}
+
+		setSearchResults(sortedResults)
+	}
 
 
 	return (<View style={styles.root}>
@@ -129,14 +184,13 @@ const SearchMap = () => {
 							  latitude: selectedPlace.latitude, longitude: selectedPlace.longitude,
 						  }}
 						/>)}
-						{/*<Circle center={{*/}
-						{/*	latitude: 44.933393, longitude: 4.892703,*/}
-						{/*}}*/}
-						{/*        radius={500}*/}
-						{/*        fillColor="rgba(255, 0, 0, 0.1)"*/}
-						{/*        strokeColor="rgba(255, 0, 0, 0.1)"*/}
-						{/*        strokeWidth={1}*/}
-						{/*/>*/}
+						{searchResults.map((person, index) => (<Circle
+						  key={index}
+						  center={person.map.coordinates}
+						  radius={person.map.radius}
+						  strokeColor={"#FF7F50"}
+						  fillColor={"rgba(255,127,80,0.35)"}
+						/>))}
 					</MapView>
 
 					<GooglePlacesAutocomplete
@@ -159,6 +213,7 @@ const SearchMap = () => {
 				  index={1}
 				  snapPoints={snapPoints}
 				  onChange={handleSheetChanges}
+				  enableContentPanningGesture={true}
 				>
 					<View style={styles.contentContainer}>
 
@@ -223,8 +278,7 @@ const SearchMap = () => {
 										color={"#FF7F50"}
 									  />
 									  <Text>{item.label}</Text>
-								  </View>
-								))}
+								  </View>))}
 							</View>
 
 							<CustomButton
@@ -250,10 +304,25 @@ const SearchMap = () => {
 									       style={styles.titleWithImageIcon} />
 									<Text style={styles.titleWithImageText}>{t("common.result.other")}</Text>
 								</View>
+
+								<Picker
+								  onValueChange={(itemValue) => sortResults(itemValue)}
+								  style={{ height: "auto", width: "auto", backgroundColor: "orange" }}
+								>
+									<Picker.Item label="Défaut" value="stars" />
+									<Picker.Item label={t("common.stars")} value="stars" />
+									<Picker.Item label={t("common.prix")} value="price" />
+									<Picker.Item label={t("common.distance")} value="distance" />
+								</Picker>
+
 							</View>
-							<View>
-								<SearchResultPerson/>
-							</View>
+							<ScrollView style={styles.searchResults} contentContainerStyle={{ flexGrow: 1 }}>
+								{searchResults.map((person, index) => (
+								  <View key={index}>
+									  <SearchResultPerson data={person} />
+								  </View>
+								))}
+							</ScrollView>
 						</View>) : null}
 					</View>
 				</BottomSheetModal>
@@ -268,8 +337,7 @@ const styles = StyleSheet.create({
 	}, mapContainer: {
 		height: "100%", width: "100%", justifyContent: "flex-end", alignItems: "center",
 	}, map: {
-		width: "100%", height: "100%",
-		top: 0, left: 0, right: 0, bottom: 0, position: "absolute",
+		width: "100%", height: "100%", top: 0, left: 0, right: 0, bottom: 0, position: "absolute",
 	}, searchBar: {
 		container: {
 			position: "absolute", top: 50, left: 10, right: 10, borderRadius: 20, shadowColor: "#000", elevation: 10,
@@ -286,8 +354,7 @@ const styles = StyleSheet.create({
 		}, separator: {
 			height: 0.5, backgroundColor: "#3400fd",
 		},
-	},
-	detailsBox: {
+	}, detailsBox: {
 		position: "absolute",
 		bottom: 0,
 		left: 0,
@@ -297,8 +364,8 @@ const styles = StyleSheet.create({
 		borderTopLeftRadius: 20,
 		borderTopRightRadius: 20,
 		paddingTop: 25,
-		paddingLeft: 25,
-		paddingRight: 25,
+		paddingLeft: 20,
+		paddingRight: 20,
 		paddingBottom: 10,
 		display: "flex",
 		justifyContent: "space-between",
@@ -310,16 +377,12 @@ const styles = StyleSheet.create({
 		width: 25, height: 25,
 	}, subtitle: {
 		marginTop: 7,
-	},
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		backgroundColor: "grey",
-	},
-	contentContainer: {
-		flex: 1,
-		alignItems: "center",
-	},
+	}, container: {
+		flex: 1, justifyContent: "center", backgroundColor: "grey",
+	}, contentContainer: {
+		flex: 1, alignItems: "center",
+	}, searchResults: undefined,
+
 })
 
 export default SearchMap
